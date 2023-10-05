04338959ed91d38382ce4bb29d1ea2398d3e3fb3
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-10-05 21:14:08 +0530

    update data using state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
+  // let data = "manisha";
 
-  let data = 'manisha'
-
+  const [data, setData] = useState('Manisha');
   const updateData = () => {
-    data = 'nayak'
-    alert(data)
-  }
+    // data = "nayak";
+    // alert(data);
+    setData('Nayak')
+  };
 
   return (
     <div className="App">
-   <h1>State</h1>
-   <h2>{data}</h2>
-   <button onClick={updateData}>Update Data</button>
+      <h1>State</h1>
+      <h2>{data}</h2>
+      <button onClick={updateData}>Update Data</button>
     </div>
   );
 }
